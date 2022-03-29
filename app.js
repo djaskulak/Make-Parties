bba3004bbd059a4ef74ad5cb12347a2df2cc5bf5
djaskulak <danikaherminise@gmail.com>
2022-03-28 17:03:14 -0700

    index events

--- app.js
+++ app.js
@@ -14,8 +14,20 @@ app.set('view engine', 'handlebars');
 app.set("views", "./views");
 
 // Render the "home" layout for the main page and send the following msg
+// app.get('/', (req, res) => {
+//   res.render('home', { msg: 'Handlebars are Cool!' });
+// })
+
+// OUR MOCK ARRAY OF PROJECTS
+var events = [
+  { title: "I am your first event", desc: "A great event that is super fun to look at and good", imgUrl: "https://laschoolfordogs.com/wp-content/uploads/2020/12/German-Shepherd-Puppy-Fetch.jpg" },
+  { title: "I am your second event", desc: "A great event that is super fun to look at and good", imgUrl: "https://laschoolfordogs.com/wp-content/uploads/2020/12/German-Shepherd-Puppy-Fetch.jpg" },
+  { title: "I am your third event", desc: "A great event that is super fun to look at and good", imgUrl: "https://laschoolfordogs.com/wp-content/uploads/2020/12/German-Shepherd-Puppy-Fetch.jpg" }
+]
+
+// INDEX
 app.get('/', (req, res) => {
-  res.render('home', { msg: 'Handlebars are Cool!' });
+  res.render('events-index', { events: events });
 })
 
 // Choose a port to listen on
